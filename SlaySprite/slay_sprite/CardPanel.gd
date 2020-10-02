3d14f467ae89b6a1abf059722ba9c6600e142f12
Nix <57319138@qq.com>
2020-10-02 16:53:09 +0800

    Card

--- SlaySprite/slay_sprite/CardPanel.gd
+++ SlaySprite/slay_sprite/CardPanel.gd
@@ -1,8 +1,20 @@
 extends Panel
 
+var rarity_dict = {
+	"Starter":Color.gray,
+	"Common":Color.green,
+	"Uncommon":Color.azure,
+	"Rare":Color.gold,
+}
+
 func Set(card):
 	$Cost/Cost.text = str(card.get("cost",0))
+	$Frame.self_modulate = rarity_dict[card.get("rarity","Starter")]
 	$Frame/Name.text = card.name
 	$Image.texture = GlbDb.CardImage(card.img)
 	$Type/Label.text = card.type
 	$Desc.text = PoolStringArray(card.desc).join("\n")
+
+func Upgrade():
+	#TODO
+	pass
