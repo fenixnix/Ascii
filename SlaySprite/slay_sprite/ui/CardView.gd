0309fed1336d91fc11abf1d538cce731a8b12c58
Nix <57319138@qq.com>
2020-10-09 17:12:45 +0800

    panel

--- SlaySprite/slay_sprite/ui/CardView.gd
+++ SlaySprite/slay_sprite/ui/CardView.gd
@@ -8,7 +8,8 @@ func Set(card_list):
 	$List.clear()
 	for card in data:
 		$List.add_item(card.name)
-	_on_List_item_selected(0)
+	if len(card_list)>0:
+		_on_List_item_selected(0)
 
 func _on_OK_pressed():
 	queue_free()
